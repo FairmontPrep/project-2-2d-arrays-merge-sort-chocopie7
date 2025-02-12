ed03ef0c9eb57753938044a10f09503e1d7caefb
Ryan <142356165+chocopie7@users.noreply.github.com>
2025-02-11 20:07:55 -0800

    Revised

--- GameBoard.java
+++ GameBoard.java
@@ -1,5 +1,5 @@
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
 public class GameBoard extends JFrame {
     private static final int SIZE = 8;
@@ -22,10 +22,31 @@ public class GameBoard extends JFrame {
         // your 2D array must be a minimum of 6 rows x 2 columns
         // you may add a row for every image if you'd like to have every square be a different color/image
 
-        piecesArray = new String[1][2];
-        piecesArray[0][0]= "temp2.png";
-        piecesArray[0][1]= "HP:200";
+        piecesArray = new String[32][2];
+        piecesArray[0]= new String[] {"B_Bishop.png","26"};
+        piecesArray[1]= new String[] {"B_Bishop.png","29"};
+        piecesArray[2]= new String[] {"B_King.png","28"};
+        piecesArray[3]= new String[] {"B_Knight.png","25"};
+        piecesArray[4]= new String[] {"B_Knight.png","30"};
+        for (int i = 5; i<5+8;i++){
+            piecesArray[i]= new String[] {"B_Pawn.png",(10+i)+""};
+        }
+        piecesArray[13]= new String[] {"B_Queen.png","28"};
+        piecesArray[14]= new String[] {"B_Rook.png","24"};
+        piecesArray[15]= new String[] {"B_Rook.png","31"};
 
+
+        piecesArray[16]= new String[] {"W_Bishop.png","2"};
+        piecesArray[17]= new String[] {"W_Bishop.png","5"};
+        piecesArray[18]= new String[] {"W_King.png","4"};
+        piecesArray[19]= new String[] {"W_Knight.png","1"};
+        piecesArray[20]= new String[] {"W_Knight.png","6"};
+        for (int i = 21; i<21+8;i++){
+            piecesArray[i]= new String[] {"W_Pawn.png",(i-5)+""};
+        }
+        piecesArray[29]= new String[] {"W_Queen.png","3"};
+        piecesArray[30]= new String[] {"W_Rook.png","1"};
+        piecesArray[31]= new String[] {"W_Rook.png","7"};
         //print the contents of your 2D array
         //this is a requirement to show your 2D array is not sorted at the beginning of your program
 
